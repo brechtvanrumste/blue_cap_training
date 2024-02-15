97fbe987b098e679276edba38c8f7f99b41a3a11
brechtvanrumste <vanrumste.brecht@gmail.com>
2024-02-15 13:52:44 +0100

    progress in exercise

--- blue_cap_training/src/test/java/com/example/BookServiceUnitTest.java
+++ blue_cap_training/src/test/java/com/example/BookServiceUnitTest.java
@@ -2,11 +2,10 @@ package com.example;
 
 
 import com.example.models.Book;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.example.services.BookService;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
