da360acec96a9e8cddbfb3187ff894fc14376bfa
brechtvanrumste <vanrumste.brecht@gmail.com>
2024-02-15 11:58:01 +0100

    progress in exercise

--- blue_cap_training/src/test/java/com/example/BookServiceUnitTest.java
+++ blue_cap_training/src/test/java/com/example/BookServiceUnitTest.java
@@ -1,19 +1,17 @@
 package com.example;
 
 
+import com.example.models.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import services.BookService;
-
+import com.example.services.BookService;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class BookServiceUnitTest {
 
     @Autowired
@@ -22,7 +20,6 @@ public class BookServiceUnitTest {
     @Test
     public void whenApplicationStarts_thenHibernateCreatesInitialRecords() {
         List<Book> books = bookService.list();
-
         assertEquals(books.size(), 3);
     }
 }
